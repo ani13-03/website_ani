0857215b47efcc8797e319c5b699496e50c18aa8
ani13-03 <anirahmayani784@gmail.com>
2025-12-17 10:06:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Untuk mengaktifkan menu hamburger //
-const hamburger = document.getElementById('hamburger');
-const menuNav = document.getElementById('menuNav');
-
-hamburger.addEventListener('click', function() {
-    menuNav.classList.toggle('active');
-    hamburger.classList.toggle('is-active');
-});
